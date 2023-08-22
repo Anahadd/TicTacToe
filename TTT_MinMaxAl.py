76fe3e9668af6cb78d95c586d78e928d2c416c2c
Anahadd <142919181+Anahadd@users.noreply.github.com>
2023-08-22 11:59:20 -0400

    Update TTT_MinMaxAl.py
    1.1 includes: 
    1. Creating The Board 
    2. Checking Win Condition 
    
    1.2 will include: 
    3. Function For Player Moves using Indexing
    4. Implementing Min-Max

--- TTT_MinMaxAl.py
+++ TTT_MinMaxAl.py
@@ -1,1 +1,21 @@
+# update 1.1 to tic tac toe using min-max algorithm
+import numpy as np
+
+def create_empty_board():
+    return np.zeros((3, 3), dtype=int)
+
+def print_board(board):
+    print(board)
+
+def check_winner(board, player):
+
+    for i in range(0, 3): # row, column
+        if np.all(board[i, :] == player) or np.all(board[:, i] == player): # checks for all horziontals, verticals
+            return True
+    if np.all(np.diag(board) == player) or np.all(np.diag(np.fliplr(board)) == player): # checks for all diagonals
+        return True
+    return False
+
+board = create_empty_board()
+print_board(board)
 
